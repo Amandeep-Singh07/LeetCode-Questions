a9302a3b32b57da377dc82b2b97bd6e118638e9a
Amandeep Singh <amandeepronaldo@gmail.com>
2025-06-07 20:44:43 +0530

    1009-Complement of Base 10 Integer

---
+++ June 2025/1009- Complement of Base 10 Integer.cpp
@@ -0,0 +1,25 @@
+// Time Complexity of O(log(n))
+class Solution {
+    public:
+        int bitwiseComplement(int n) {
+    
+            
+            int temp=n;
+            int mask=0;
+    
+            if(temp ==0){
+                return 1;
+            }
+    
+            while(temp!=0){
+                
+              mask = (mask<<1) | 1;
+              temp= temp>>1;
+            }
+            
+            int ans=(~n)& mask;
+            return ans;
+    
+            
+        }
+    };
